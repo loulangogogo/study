4fb158a6b7dda453b1434ea127e9f2150049309b
loulan <loulangogogo@163.com>
2025-07-02 17:42:18 +0800

    feat(Book-C_classics): 添加 goto 语句示例并优化代码结构
    - 在 F3.2.1.c 文件中添加了 goto 语句的使用示例
    - 通过 goto 语句改变了程序的执行流程，跳过了部分

--- Book-C_classics/F3/F3.2.1.c
+++ Book-C_classics/F3/F3.2.1.c
@@ -5,6 +5,10 @@ int main()
     unsigned short a = 4;
     printf("%d\n", a);
     printf("%d\n", !a);
+    goto sss;
     printf("%d\n", ~a);
+    printf("%d\n", 456);
+sss:
+    printf("%d\n", 123);
     return 0;
 }
